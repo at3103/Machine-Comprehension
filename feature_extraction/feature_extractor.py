6531977aff7b173e54fa3c555955a2002ef30e26
Ashwin Karthik Tamilselvan <at3103@columbia.edu>
2016-12-04 01:21:11 -0500

    included provision for multiple questions

--- feature_extraction/feature_extractor.py
+++ feature_extraction/feature_extractor.py
@@ -13,20 +13,23 @@ def parse_json(filename):
         if os.path.isfile(filename + ext):
             with open(filename + ext) as json_file:
                 file_data = json.load(json_file)
+                d = []
                 if i == 1:
-                    sentences = file_data['questions']['sentences']
+                    for q in file_data['questions']:
+                        d.append(q['sentences'])
                 else:
-                    sentences = file_data['sentences']
+                    d.append(file_data['sentences'])
                 pos = []
                 tokens = []
                 parse = []
                 constituents = []
-                for sentence in sentences:
-                    pos.append(sentence['pos'])
-                    tokens.append(sentence['tokens'])
-                    parse.append(sentence['parse'])
-                    constituents.append(sentence['constituents'])
-                elements.append([pos,tokens,parse,constituents])
+                for sentences in d:
+                    for sentence in sentences:
+                        pos.append(sentence['pos'])
+                        tokens.append(sentence['tokens'])
+                        parse.append(sentence['parse'])
+                        constituents.append(sentence['constituents'])
+                    elements.append([pos,tokens,parse,constituents])
         else:
             elements.append([])
         i += 1
