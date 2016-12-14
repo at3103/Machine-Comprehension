76fb5e2b295b9ef564c53acc9576d0083caafe60
Ashwin Karthik Tamilselvan <at3103@columbia.edu>
2016-12-14 07:08:28 -0500

    Increased train:test split

--- Machine Learning/GBR.py
+++ Machine Learning/GBR.py
@@ -23,37 +23,46 @@ from sklearn.linear_model import SGDRegressor
 import pandas
 import numpy as np
 import os
+from os import listdir
+from os.path import isfile, join
 
 n_x = 22	#Columns which are considered features
 n_y = 22 # the column for label
 
 # Load dataset
-frames = []
-for i in xrange(1,38):
-	url = "../data/featuredata/{0}.csv".format(i)
+# Load dataset
+frames=[]
+data_file_path = "../data/featuredata_br/"
+data_files = [f for f in listdir(data_file_path) if isfile(join(data_file_path, f)) and f.endswith('.csv')]
+for i in data_files:
+	url = data_file_path + i
 	# names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-	dataset = pandas.read_csv(url)#, names=names)
+	dataset = pd.read_csv(url)#, names=names)
 	frames.append(dataset)
 	final_dataset = pd.concat(frames)
 
 #Different buil-in functions of ndarray
-print dataset.shape, dataset.ndim, len(dataset.shape) #, dataset.size#, dataset.dtype, dataset.itemsize
+print final_dataset.shape, final_dataset.ndim, len(final_dataset.shape) #, dataset.size#, dataset.dtype, dataset.itemsize
 
 #Extracting the values from the dataframe
 array = final_dataset.values
 
 #Separating the features and the labels
+array = final_dataset.values
+#print array[0]
+#Separating the features and the labels
 X = array[:,1:]
 Y = array[:,n_y]
 qid = array[:,24]
 #print X[0], Y[0], qid[0]
-#Validation Size
-test_size = 0.3
+
 
 #Set the seed for randomness here
 seed = 7
 gkf = GroupKFold(n_splits=2)
 #Obtain the training and test sets
+#X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X_qid_uniq,
+#	test_size = test_size, random_state = seed)
 X_train = []
 X_test = []
 Y_train = []
@@ -62,9 +71,24 @@ Y_test = []
 splits = gkf.split(X,Y,groups = qid)
 cur_splits = splits.next()
 X_train = X[cur_splits[0]]
-X_test = X[cur_splits[1]]
+X_test1 = X[cur_splits[1]]
 Y_train = Y[cur_splits[0]]
-Y_test = Y[cur_splits[1]]
+Y_test1 = Y[cur_splits[1]]
+
+qid=X_test1[:,24]
+splits = gkf.split(X_test1,Y_test1,groups = qid)
+cur_splits = splits.next()
+
+print X_train.shape, X_test1[cur_splits[0]].shape
+print Y_train.shape, Y_test1[cur_splits[0]].shape
+
+X_train = (np.concatenate((X_train.T, X_test1[cur_splits[0]].T),axis = 1)).T
+X_test = X_test1[cur_splits[1]]
+Y_train = (np.concatenate((Y_train,Y_test1[cur_splits[0]])))
+Y_test = Y_test1[cur_splits[1]]
+
+print X_train.shape, X_test.shape
+print Y_train.shape, Y_test.shape
 
 #Enable for labels
 
